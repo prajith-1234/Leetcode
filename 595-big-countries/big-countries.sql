554304008d4deb1f424bbb123bc68c8bcff90e47
prajith-1234 <prajithudhaya143@gmail.com>
2026-08-09 00:10:40 +0530

    Time: 322 ms (37.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 595-big-countries/big-countries.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select w.name , w.population , w.area
+from world w
+
+having w.population >= 25000000 or w.area >=3000000 or w.population is null and w.area is null;
